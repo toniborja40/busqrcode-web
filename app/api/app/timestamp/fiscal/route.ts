dca41cda2d50f50ac4dd4ad1058b26868ebe2736
daws-4 <villamizarandresdavid@gmail.com>
2025-02-03 11:52:01 -0400

    03-02-2025 -4

--- app/api/app/timestamp/fiscal/route.ts
+++ app/api/app/timestamp/fiscal/route.ts
@@ -9,8 +9,25 @@ export async function POST(request: any) {
     const {numero_fiscal, id_fiscal} = await request.json();
     console.log(numero_fiscal, id_fiscal);
     try {
-        
-        return NextResponse.json({ numero_fiscal, id_fiscal }, { status: 200 });
+      // Obtener la fecha de hoy
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      const tomorrow = new Date(today);
+      tomorrow.setDate(today.getDate() + 1);
+
+      // Buscar registros de timestamps donde createdAt esté dentro del rango del día de hoy y id_fiscal sea igual a id_fiscal
+      const registros = await timestamps.find({
+        createdAt: {
+          $gte: today,
+          $lt: tomorrow,
+        },
+        id_fiscal: id_fiscal,
+      });
+
+           return NextResponse.json(
+             { numero_fiscal, id_fiscal, registros },
+             { status: 200 }
+           );
     } catch (error) {
         console.log(error);
         return NextResponse.json((error as Error).message, { status: 400 });
