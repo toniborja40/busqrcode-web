2d539c86cb667c0aebd33a7e35699074a8f14283
daws-4 <villamizarandresdavid@gmail.com>
2025-02-03 11:35:01 -0400

    03-02-2025 -2

--- app/api/app/timestamp/fiscal/route.ts
+++ app/api/app/timestamp/fiscal/route.ts
@@ -6,7 +6,14 @@ import { NextResponse } from "next/server";
 connectDB();
 
 export async function POST(request: any) {
-        const {numero_fiscal, id_fiscal} = await request.json();
-        console.log(numero_fiscal, id_fiscal);
-    
+    const {numero_fiscal, id_fiscal} = await request.json();
+    console.log(numero_fiscal, id_fiscal);
+    try {
+        
+        return NextResponse.json({ numero_fiscal, id_fiscal }, { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json((error as Error).message, { status: 400 });
+        
+    }
 }
